477f52ef6f530dddc2096ea8f9304e330a25d850
eric allibalogun <allieric28@gmail.com>
2026-03-03 16:27:22 +0100

    docker fix

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /opt/keycloak/bin/kc.sh start --http-port=${PORT:-8080} --hostname-strict=false --proxy=edge
+exec /opt/keycloak/bin/kc.sh start --optimized --http-port=${PORT:-8080} --hostname-strict=false --proxy=edge
